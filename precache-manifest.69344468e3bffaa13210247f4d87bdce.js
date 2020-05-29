3fdf9810c07895a0e96461a8fe61f9f9980a4c9c
maore <maorelfassy@gmail.com>
2020-05-29 20:23:05 +0300

    Updates

---
+++ precache-manifest.69344468e3bffaa13210247f4d87bdce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9729f571d1feab81b8f099b51be09db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70ebbea0fa487702428",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b83ab471a1efb66f2bee",
+    "url": "/static/js/2.39c85313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39c85313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70ebbea0fa487702428",
+    "url": "/static/js/main.10f93ce0.chunk.js"
+  },
+  {
+    "revision": "f525e04183c2cf92a9e5",
+    "url": "/static/js/runtime-main.8b3acbdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
